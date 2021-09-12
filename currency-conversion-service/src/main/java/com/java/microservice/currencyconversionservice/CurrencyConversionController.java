90b9bf20fac1d6de966a0604843f6b120eee8ccc
Rohit444 <rohitsharma8650@gmail.com>
2021-09-12 21:45:03 +0530

    Added Currency-Conversion-Service
    Added Currency-Conversion-SpringBoot-Service

---
+++ currency-conversion-service/src/main/java/com/java/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,52 @@
+package com.java.microservice.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionJdbcTemplet(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		HashMap<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		// The getForEntity method retrieves resources from the given URI or URL
+		// templates. It returns response as ResponseEntity using which we can get
+		// response status code, response body etc
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" "+"RestTemplet");
+	}
+
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+				currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+" "+"Feign");
+	}
+	
+}
---
+++ currency-conversion-service/src/test/java/com/java/demo/reatelimiter/ratelimiterdemo/RatelimiterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.demo.reatelimiter.ratelimiterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
